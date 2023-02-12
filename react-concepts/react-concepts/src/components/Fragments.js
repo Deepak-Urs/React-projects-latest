a2fa7fd1f1c2029cc821428986730ae8d282acf4
Deepak-Urs <gvdeepakurs2202@gmail.com>
2023-02-11 18:52:11 -0800

    Fragments

---
+++ react-concepts/react-concepts/src/components/Fragments.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Fragments() {
+  return (
+    <React.Fragment>
+      <div>Hello</div>
+      <div>World</div>
+    </React.Fragment>
+  )
+}
+
+export default Fragments
